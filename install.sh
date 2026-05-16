6240076e80f2b27c5379dbe084b474850297e924
1mvghost <55602875+1mvghost@users.noreply.github.com>
2026-05-16 18:10:13 +0200

    limine update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt install nasm
 sudo apt install qemu-system
 sudo apt install xorriso
-git clone https://codeberg.org/Limine/limine.git --branch=v10.x-binary --depth=1
+git clone https://codeberg.org/Limine/limine.git --branch=v11.x-binary --depth=1
