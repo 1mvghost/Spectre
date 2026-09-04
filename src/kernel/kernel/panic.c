46e7539efa0b2fe597b57494814a0b1419c55c2c
1mvghost <55602875+1mvghost@users.noreply.github.com>
2026-09-05 01:27:47 +0200

    panic: fixed printf sizes

--- src/kernel/kernel/panic.c
+++ src/kernel/kernel/panic.c
@@ -36,13 +36,15 @@ static const char* exceptions[32] = {"Div By Zero",
 void panicIsr(Regs* regs) {
   printf("%s\n", exceptions[regs->intId]);
   printf("STOP:%x INT:%x\n", regs->errId, regs->intId);
-  printf("RAX:%x RBX:%x RCX:%x RDX:%x RSP:%x RDI:%x RSI:%x\n", regs->rax,
-         regs->rbx, regs->rcx, regs->rdx, regs->rsp, regs->rdi, regs->rsi);
-  printf("R8:%x R9:%x R10:%x R11:%x R12:%x R13:%x R14:%x R15:%x\n", regs->r8,
-         regs->r9, regs->r10, regs->r11, regs->r12, regs->r13, regs->r14,
-         regs->r15);
-  printf("RIP:%x CS:%x RFLAGS:%x\n", regs->rip, regs->cs, regs->rFlags);
-  printf("SS:%x KRSP:%x\n", regs->ss, regs->kRsp);
+  printf("RAX:%llx RBX:%llx RCX:%llx RDX:%llx RSP:%llx RDI:%llx RSI:%llx\n",
+         regs->rax, regs->rbx, regs->rcx, regs->rdx, regs->rsp, regs->rdi,
+         regs->rsi);
+  printf(
+      "R8:%llx R9:ll%x R10:%llx R11:%llx R12:%llx R13:%llx R14:%llx R15:%llx\n",
+      regs->r8, regs->r9, regs->r10, regs->r11, regs->r12, regs->r13, regs->r14,
+      regs->r15);
+  printf("RIP:%llx CS:%llx RFLAGS:%llx\n", regs->rip, regs->cs, regs->rFlags);
+  printf("SS:%llx KRSP:%llx\n", regs->ss, regs->kRsp);
   panic("");
 }
 
